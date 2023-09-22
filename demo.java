65c7f0e36e45b74290c38764e2d20c21e08fbc87
Nikhil Palve <nikhilpalve18@gmail.com>
2023-09-23 00:32:44 +0530

    This is 2nd commit

--- demo.java
+++ demo.java
@@ -1,5 +1,6 @@
 public class demo{
     public static void main(String[] args) {
         System.out.println("Hello this is Nikhil palve");
+        System.out.println("This is my 1st commit");
     }
 }
