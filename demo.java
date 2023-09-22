0b2361ee6e25365b27a766103de4846414beff7f
Nikhil Palve <nikhilpalve18@gmail.com>
2023-09-23 00:41:29 +0530

    jenkins commit

--- demo.java
+++ demo.java
@@ -2,5 +2,7 @@ public class demo{
     public static void main(String[] args) {
         System.out.println("Hello this is Nikhil palve");
         System.out.println("This is my 1st commit");
+        System.out.println("This is my 2nd commit will reflect onto jenkins");
+
     }
 }
