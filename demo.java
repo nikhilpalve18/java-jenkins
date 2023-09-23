895cb681927bee7efd22d59c0144de445673bea1
Nikhil Palve <81505833+nikhilpalve18@users.noreply.github.com>
2023-09-23 11:07:05 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,8 +1,6 @@
 public class demo{
     public static void main(String[] args) {
         System.out.println("Hello this is Nikhil palve");
-        System.out.println("This is my 1st commit");
-        System.out.println("This is my 2nd commit will reflect onto jenkins");
-
+        System.out.println("Hello I am Nikhil Palve from NelsoniQ");
     }
 }
